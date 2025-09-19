d0e6998ca8b642212329c1781354ccea60b56a81
Breno Estrella <brenogbestrella@gmail.com>
2025-09-19 13:11:57 -0300

    Update counter.cljs

--- src/views/counter.cljs
+++ src/views/counter.cljs
@@ -28,17 +28,3 @@
      [:div.buttons
       [:button {:on-click increment-counter!} "Increment"]
       [:button {:on-click reset-counter!} "Reset"]]]))
-
-
-
-
-;; (defn -main []
-;;   (println "Olá"))
-
-;; (defn main-panel []
-;;   (let [counter (re-frame/subscribe [::subs/counter])]
-;;     [:div
-;;      [:h1 "Counter"]
-;;      [:p "Current value: " @counter]
-;;      [:button {:on-click #(re-frame/dispatch [::events/increment-counter])}
-;;       "Increment"]]))  
